4a3943992195c51307b502d9c6c806d225544bb4
pataspatek <patrikpatek3@gmail.com>
2025-11-29 11:58:04 +0100

    todo model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title: models.CharField = models.CharField(max_length=64)
+    description: models.CharField = models.CharField(max_length=255, blank=True)
+    completed: models.BooleanField = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
