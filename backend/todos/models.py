699b8a79ce2004bc45fc36b998e4c3f09d62e33b
pataspatek <patrikpatek3@gmail.com>
2025-12-03 10:05:00 +0100

    article 05 - admin panel

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -6,4 +6,4 @@ class Todo(models.Model):
     completed: models.BooleanField = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return f"{self.id} - {self.title}"
