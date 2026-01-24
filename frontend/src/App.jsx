d68f9f69e33fae1b6b600df758ac710c960f86d8
pataspatek <patrikpatek3@gmail.com>
2026-01-24 20:04:19 +0100

    404 route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
                 <Route path="/todos" element={<h1>Todos</h1>} />
+                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
             </Routes>
         </BrowserRouter>
     );
