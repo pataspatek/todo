9cb6198c31d1a94b83ced790bd4a79a2b84197f1
pataspatek <patrikpatek3@gmail.com>
2026-02-15 20:46:55 +0100

    React API call

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todos'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/views/todos/TodosView.jsx
+++ frontend/src/views/todos/TodosView.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+
 function TodosView() {
+    const [todos, setTodos] = useState([]);
+
+    const fetchTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:8000/todos/");
+            const data = await response.json();
+            setTodos(data)
+        } catch (error) {
+            console.error("Error fetching todos:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTodos();
+    }, [])
+    
     return (
         <div>
             <h1>Todos</h1>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        <h3>
+                            {todo.title}
+                            <span>{todo.completed ? "✅" : "❌"}</span>
+                        </h3>
+                        <p>{todo.description}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
