7cc7438d9da5b817f0f73c428778c626eb733b7d
pataspatek <patrikpatek3@gmail.com>
2026-02-16 22:01:41 +0100

    13 in progress

--- frontend/src/views/todos/TodosView.jsx
+++ frontend/src/views/todos/TodosView.jsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react";
 
 function TodosView() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
     const [todos, setTodos] = useState([]);
 
     const fetchTodos = async () => {
         try {
-            const response = await fetch("http://localhost:8000/todos/");
+            const response = await fetch("http://localhost:8000/todos/", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
             const data = await response.json();
             setTodos(data)
         } catch (error) {
             console.error("Error fetching todos:", error);
         }
     };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(title)
+        console.log(description)
+    }
 
     useEffect(() => {
         fetchTodos();
@@ -31,6 +47,23 @@ function TodosView() {
                     </li>
                 ))}
             </ul>
+
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Description"    
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <button type="submit">Add Todo</button>
+            </form>
+
         </div>
     );
 }
